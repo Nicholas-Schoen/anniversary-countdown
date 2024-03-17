52bddee6b2d20ea769e8d46de38c791476059a2e
Nicholas Schoen <scheontransit@gmail.com>
2024-03-17 17:35:08 -0500

    wrote javascript for the timer, also updated css and html to play nice with the js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const anniversaryDayMessage = document.getElementById("anniversary-day");
+const mainPage = document.getElementById("count-down")
 const tagLine = document.getElementById("tag-line");
-const monthsNumber = document.getElementById("months-number")
 const daysNumber = document.getElementById("days-number");
 const hoursNumber = document.getElementById("hours-number");
 const minutesNumber = document.getElementById("minutes-number")
@@ -9,20 +9,29 @@ const secondsNumber = document.getElementById("seconds-number")
 const date = new Date();
 const year = date.getFullYear();
 const anniversaryDate = new Date(`December 15, ${year} 00:00:00`);
-const now = date.getTime();
-
-let distance = anniversaryDate - now;
 
 function countdownTimer() {
-  
+  const now = new Date().getTime();
+  const distance = anniversaryDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  daysNumber.innerText = days;
+  hoursNumber.innerText = hours;
+  minutesNumber.innerText = minutes;
+  secondsNumber.innerText = seconds;
+
+  if (distance <= 0){
+    clearInterval(timer);
+    anniversaryDayMessage.classList.toggle("hidden");
+    mainPage.classList.toggle("hidden")
+
+  }
 };
-/*
 
-while(distance > 0){
-    countdownTimer();
-};
+var timer = setInterval(countdownTimer, 1000);
 
-*/
-
-console.log(distance);
 
